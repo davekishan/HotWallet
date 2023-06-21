583d65e331e9205dfec05d1c1c9510cee4c77bc2
davekishan <75985572+davekishan@users.noreply.github.com>
2023-06-21 12:06:55 +0530

    create navbar

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Navbar = () => {
+  const navigate=useNavigate();
+  const [loader, setloader] = useState(false);
+
+  const logout=()=>{
+    fetch('/api/login/logout').then(response => response.json())
+    .then((data) => {
+      if (data.success) {
+        toast.success('Loged Out')
+        setloader(false)
+        navigate('/')
+      }else{
+        toast.error('Failed')
+      }
+    })
+
+  }
+
   return (
-    <div>Navbar</div>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <NavLink className="navbar-brand" to='/home'>Navbar</NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink className="nav-item nav-link" to='/home'>Home <span className="sr-only"></span></NavLink>
+            <NavLink className="nav-item nav-link" to='/home'>Send</NavLink>
+
+          </div>
+        </div>
+        <button className='btn btn-primary mx-5' onClick={logout}>Logout</button>
+      </nav>
+      <ToastContainer />
+
+    </div>
   )
 }

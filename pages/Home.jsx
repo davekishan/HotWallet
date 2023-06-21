453766f14ea3652eae779099139287cab779ee53
davekishan <75985572+davekishan@users.noreply.github.com>
2023-06-21 12:07:42 +0530

    add footer css

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Navbar } from '../component/Navbar'
+import { useNavigate } from 'react-router-dom';
+import { Footer } from '../component/footer';
 
 export const Home = () => {
+  const [loader, setloader] = useState(false);
+  const navigate=useNavigate();
+
+  useEffect(() => {
+    setloader(true)
+
+    fetch('/api/login/checksession').then(response => response.json())
+      .then((data) => {
+        if (data.success) {
+            
+          setloader(false)
+          navigate('/home')
+        }else{
+          navigate('/')
+        }
+      })
+    setloader(false)
+  }, [])
+
   return (
-    <div><h1 style={{color:'white'}}>Welcome Home</h1></div>
+    <div>
+      <div>
+        <Navbar/>
+      </div>
+      <h1 style={{color:'white'}}>Welcome Home</h1>
+
+        <Footer/>
+      </div>
   )
 }

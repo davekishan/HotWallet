c13a7bae3dd4fc0f7913f8d803f438adefcc6355
davekishan <75985572+davekishan@users.noreply.github.com>
2023-06-30 19:31:09 +0530

    add firebase

---
+++ public/firebase-messaging-sw.jsx
@@ -0,0 +1,8 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js'
+
+
+firebase.initializeApp(
+   { messagingSenderId: "9716179048"}
+)
+
+const initMessaging=firebase.messaging();

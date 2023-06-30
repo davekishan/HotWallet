a39565ad88b2dc8fb3801e8c1c72ed2d2797ecaf
davekishan <75985572+davekishan@users.noreply.github.com>
2023-06-30 19:31:37 +0530

    add firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+
+import firebase from 'firebase/compat/app';
+const config={  
+
+    apiKey: "AIzaSyBMkA0XYzaFx1y4sXQW4M5mZvN4gJUSm2A",
+    authDomain: "hot-wallet-5869c.firebaseapp.com",
+    projectId: "hot-wallet-5869c",
+    storageBucket: "hot-wallet-5869c.appspot.com",
+    messagingSenderId: "9716179048",
+    appId: "1:9716179048:web:e10d2b198f034fe0a59bdd",
+    measurementId: "G-TE6KDZYQD1"
+
+};
+
+firebase.initializeApp(config);
+
+export default firebase
